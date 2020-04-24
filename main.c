3878beea3d612cae16a81f441320c26507940fbe
volkan <volkanisik@live.dk>
2020-04-24 19:26:20 +0200

    add funktion ER DONe

--- main.c
+++ main.c
@@ -116,7 +116,7 @@ void ADD(char input[]) {
     }
 
     int reg_bits[3] = {0, 0, 0};
-
+    //printer register DR1
     char reg1 = input[5];
     reg1 = reg1 - 48; //reset ASCII value
     regbin(reg1, -1, reg_bits);
@@ -124,7 +124,7 @@ void ADD(char input[]) {
     for (int l = 2; l >= 0; --l) {
         printf("%d", reg_bits[l]);
     }
-
+    //printer register SR1
     char reg2 = input[9];
     reg2 = reg2 - 48; //reset ASCII value
     regbin(reg2, -1, reg_bits);
@@ -133,8 +133,9 @@ void ADD(char input[]) {
         printf("%d", reg_bits[l]);
     }
 
-
+        // Her kontrolleres der om det er en immadiate værdi eller bare register
     if (input[12] == 'R') {
+
         printf("000");
 
         char reg3 = input[13];
@@ -155,29 +156,54 @@ void ADD(char input[]) {
             } else
                 break;
         }
+
+        //iMMDIAtE IDENTIFIER
+        printf("1");
+        // Læser en char array laver den om til int.
         int immadiateint;
         immadiateint = atoi(imm);
 
-        int last[5] = {0,0,0,0,0};
+        int immValue[5] = {0, 0, 0, 0, 0};
 
+
+        //Hvis der skal adderes med - så laver den tallet om til plus der efter 1's complement.
         if(imm[0]=='-'){
             immadiateint = immadiateint * -1;
-            regbin(immadiateint,-1,last);
+            regbin(immadiateint, -1, immValue);
+
+            for (int i = 0; i < 5 ; ++i) {
+                if(immValue[i] == 0)
+                    immValue[i]=1;
+                else
+                    immValue[i]=0;
+            }
+
+            // Tilføjer 1 for at lave 2's complement
+            int carrier = 1;
+            int iterate = 0;
+                do {
+                    if(immValue[iterate] == 0){
+                        immValue[iterate]=1;
+                        carrier = 0;
+                    } else
+                        immValue[iterate]=0;
+                    iterate++;
+
+                }while(carrier>0);
 
 
+            for (int l = 4; l >= 0; --l) {
+                printf("%d", immValue[l]);
+            }
 
+        }else {
 
+            regbin(immadiateint, -1, immValue);
+            for (int l = 4; l >= 0; --l) {
+                printf("%d", immValue[l]);
+            }
         }
-
-        regbin(immadiateint,-1,last);
-        for (int l = 4; l >= 0; --l) {
-            printf("%d", last[l]);
-        }
-
-
     }
-
-
 }
 
 
